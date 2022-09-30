3c46640949d619206dbfafa85458d70bb73a8fd5
smith2eric <ericsmithsonian2@gmail.com>
2022-09-30 05:51:58 +0100

    [fix]: resolved pycodestyle errors

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
 """
-Module which contains a Fabric script (based on the file 2-do_deploy_web_static.py)
+Module which contains a Fabric script
+(based on the file 2-do_deploy_web_static.py)
 that creates and distributes an archive to your web servers, using the
 function deploy:
 - Prototype: def deploy():
 - The script should take the following steps:
     - Call the do_pack() function and store the path of the created archive
     - Return False if no archive has been created
-    - Call the do_deploy(archive_path) function, using the new path of the new archive
+    - Call the do_deploy(archive_path) function,
+       using the new path of the new archive
     - Return the return value of do_deploy
 """
 
