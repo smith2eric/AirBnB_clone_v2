d2137ff45836dab32272df3785bea9d298a3772d
smith2eric <ericsmithsonian2@gmail.com>
2022-10-13 22:54:01 +0100

    [fix]: resolved errors and formatted code

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,55 +1,26 @@
 #!/usr/bin/python3
-<<<<<<< HEAD
-"""This script starts a Flask web application"""
+""" This script starts a Flask web application """
 from flask import Flask, escape
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def route():
-    """Return two words"""
+def root():
+    """ Returns Hello HBNB """
     return "Hello HBNB!"
 
 
 @app.route('/hbnb', strict_slashes=False)
-def route_hbnb():
-    """Return a word"""
+def hbnb():
+    """ Returns HBNB """
     return "HBNB"
 
 
-@app.route('/c/<path:subpath>', strict_slashes=False)
-def route_c(subpath):
-    """Return subpath"""
-    return "C {}".format(escape(subpath).replace('_', ' '))
+@app.route('/c/<path:text>', strict_slashes=False)
+def c(text):
+    """ Returns C with text passed to the URL """
+    return "C {}".format(escape(text).replace('_', ' '))
 
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000)
-=======
-"""
-starts a Flask web application
-"""
-
-from flask import Flask
-
-app = Flask(__name__)
-
-
-@app.route("/", strict_slashes=False)
-def index():
-    return "Hello HBNB!"
-
-
-@app.route("/hbnb", strict_slashes=False)
-def hbnb():
-    return "HBNB"
-
-
-@app.route("/c/<text>", strict_slashes=False)
-def C_is_fun(text):
-    return "C " + text.replace("_", " ")
-
-
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port='5000')
->>>>>>> e3cc30ab45a610fcb3b4e6a7cc759ef6443e7fa5
