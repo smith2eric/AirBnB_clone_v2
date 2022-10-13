4699734031990ebc87e7bafba3997d2a050475a0
smith2eric <ericsmithsonian2@gmail.com>
2022-10-13 22:53:27 +0100

    [fix]: resolved errors and formatted code

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,32 +1,14 @@
 #!/usr/bin/python3
-<<<<<<< HEAD
 """ This script starts a Flask web application """
 from flask import Flask
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def hello():
-    """Return two words"""
+def root():
+    """ Returns Hello HBNB """
     return "Hello HBNB!"
 
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000)
-=======
-"""
-starts a Flask web application
-"""
-from flask import Flask
-
-app = Flask(__name__)
-
-
-@app.route("/")
-def index():
-    return "Hello HBNB!"
-
-
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port='5000')
->>>>>>> e3cc30ab45a610fcb3b4e6a7cc759ef6443e7fa5
