e6c3aae8cbb028b33aae544f791df3cbef2c08a8
smith2eric <ericsmithsonian2@gmail.com>
2022-10-15 12:59:33 +0100

    [update]: resolved errors and added missing files to models and models/engine

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,6 +8,7 @@ from models.state import State
 from models.city import City
 from models.amenity import Amenity
 from models.review import Review
+import shlex
 
 classes = {'BaseModel': BaseModel, 'User': User, 'Place': Place,
            'State': State, 'City': City, 'Amenity': Amenity,
@@ -15,72 +16,69 @@ classes = {'BaseModel': BaseModel, 'User': User, 'Place': Place,
 
 
 class FileStorage:
-    """This class manages storage of hbnb models in JSON format"""
+    """This class serializes instances to a JSON file and
+    deserializes JSON file to instances
+    Attributes:
+        __file_path: path to the JSON file
+        __objects: objects will be stored
+    """
     __file_path = 'file.json'
     __objects = {}
 
     def all(self, cls=None):
-        """Returns a dictionary of models currently in storage"""
-<<<<<<< HEAD
-        if cls is None:
-            return FileStorage.__objects
-        store = {}
-        for key, value in FileStorage.__objects.items():
-            if value.__class__ == cls:
-                store[key] = value
-        return store
-=======
-
+        """returns a dictionary
+        Return:
+            returns a dictionary of __object
+        """
+        dic = {}
         if cls:
-            all = dict()
-            for i in self.__objects.keys():
-                if i.split('.')[0] == cls.__name__:
-                    all[i] = self.__objects[i]
-            
-            return all
-
-        return FileStorage.__objects
->>>>>>> e3cc30ab45a610fcb3b4e6a7cc759ef6443e7fa5
+            dictionary = self.__objects
+            for key in dictionary:
+                partition = key.replace('.', ' ')
+                partition = shlex.split(partition)
+                if (partition[0] == cls.__name__):
+                    dic[key] = self.__objects[key]
+            return (dic)
+        else:
+            return self.__objects
 
     def new(self, obj):
-        """Adds new object to storage dictionary"""
-        self.all().update({obj.to_dict()['__class__'] + '.' + obj.id: obj})
+        """sets __object to given obj
+        Args:
+            obj: given object
+        """
+        if obj:
+            key = "{}.{}".format(type(obj).__name__, obj.id)
+            self.__objects[key] = obj
 
     def save(self):
-        """Saves storage dictionary to file"""
-        temp = {}
-        with open(self.__file_path, 'w') as f:
-            temp.update(FileStorage.__objects)
-            for key, val in temp.items():
-                temp[key] = val.to_dict()
-            json.dump(temp, f)
+        """serialize the file path to JSON file path
+        """
+        my_dict = {}
+        for key, value in self.__objects.items():
+            my_dict[key] = value.to_dict()
+        with open(self.__file_path, 'w', encoding="UTF-8") as f:
+            json.dump(my_dict, f)
 
     def reload(self):
-        """Loads storage dictionary from file"""
+        """serialize the file path to JSON file path
+        """
         try:
-            with open(self.__file_path, 'r') as fo:
-                tmp = json.load(fo)
-            for key in tmp.items():
-                self.__objets[key] = classes[tmp[key]['__class__']](**tmp[key])
-        except:
+            with open(self.__file_path, 'r', encoding="UTF-8") as f:
+                for key, value in (json.load(f)).items():
+                    value = eval(value["__class__"])(**value)
+                    self.__objects[key] = value
+        except FileNotFoundError:
             pass
 
     def delete(self, obj=None):
-<<<<<<< HEAD
-        """ Delete obj from __objects if it’s inside
-        - if obj is equal to None """
+        """ delete an existing element
+        """
         if obj:
-            key = obj.to_dict()['__class__'] + '.' + obj.id
-            del FileStorage.__objects[key]
+            key = "{}.{}".format(type(obj).__name__, obj.id)
+            del self.__objects[key]
 
     def close(self):
-        """method to reload"""
+        """ calls reload()
+        """
         self.reload()
-=======
-        """delete obj from __objects if it’s inside
-        if obj is equal to None, the method should not do anything"""
-
-        if obj:
-            del(self.__objects["{}.{}".format(type(obj).__name__, obj.id)])
-            del(obj)
->>>>>>> e3cc30ab45a610fcb3b4e6a7cc759ef6443e7fa5
