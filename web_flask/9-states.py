888a1683eed5065c442a2b4c2f08498572a33679
smith2eric <ericsmithsonian2@gmail.com>
2022-10-17 08:22:04 +0100

    [fix]: resolved merge conflicts

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-<<<<<<< HEAD
 """This script starts a Flask web application"""
 from flask import Flask, escape, render_template
 from models import storage
@@ -28,39 +27,3 @@ def teardown(self):
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000)
-=======
-"""
-starts a Flask web application
-"""
-
-from models import *
-from flask import Flask, render_template
-app = Flask(__name__)
-
-
-@app.route('/states', strict_slashes=False)
-def states():
-    states = storage.all("State")
-    return render_template('9-states.html', states=states)
-
-
-@app.route('/states/<id>', strict_slashes=False)
-def states_given_id(id):
-    states = storage.all("State")
-    found_state = ""
-    for s_id in states:
-        if s_id == id:
-            found_state = states[s_id]
-
-    return render_template('9-states.html',
-                           state=found_state)
-
-
-@app.teardown_appcontext
-def teardown(err):
-    storage.close()
-
-
-if __name__ == "__main__":
-    app.run(host='0.0.0.0')
->>>>>>> e3cc30ab45a610fcb3b4e6a7cc759ef6443e7fa5
