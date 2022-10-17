674ea316d14523e829102ffe400dae418b8c0669
smith2eric <ericsmithsonian2@gmail.com>
2022-10-17 07:42:30 +0100

    [fix]: resolved errors in script and template file

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -8,10 +8,9 @@ app = Flask(__name__)
 
 @app.route('/states_list', strict_slashes=False)
 def states_list():
-    """Returns a rendered html template at the /states_list route,
-    listing all states"""
-    return render_template('7-states_list.html',
-                           states=storage.all('State').values())
+    """ display a HTML page with the states listed in alphabetical order """
+    states = sorted(list(storage.all("State").values()), key=lambda x: x.name)
+    return render_template('7-states_list.html', states=states)
 
 
 @app.teardown_appcontext
